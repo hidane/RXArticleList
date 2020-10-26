2c7ebf409f5a1aad7e5fb92bd3c18924315a5305
abhishek.s <asarnaik@softcell.com>
2020-10-26 11:44:19 +0530

    Removed Scrolling activity and added writer/user activity

---
+++ app/src/main/java/com/android/articlesdashboard/ui/view/UserActivity.kt
@@ -0,0 +1,12 @@
+package com.android.articlesdashboard.ui.view
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.android.articlesdashboard.R
+
+class UserActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_user)
+    }
+}
